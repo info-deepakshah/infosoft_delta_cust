5ef5c302a2e3f448ba197b4d9e5183e3faac4953
infosoft.deepakshah <infosoft.deepakshah@gmail.com>
2026-08-24 18:40:58 +0530

    feat(layout): apply global full-width CSS layout for forms in v16

--- infosoft_delta_cust/hooks.py
+++ infosoft_delta_cust/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/infosoft_delta_cust/css/infosoft_delta_cust.css"
+app_include_css = "/assets/infosoft_delta_cust/css/delta_global.css"
 app_include_js = "/assets/infosoft_delta_cust/js/delta_global.js"
 
 # include js, css files in header of web template
